276bff7374413521dfb871d24de628d610a19242
Adrien <adriensauvage59@gmail.com>
2023-10-06 14:44:36 +0200

    finished habitation pro

--- src/Habitation/DemoHabitation.java
+++ src/Habitation/DemoHabitation.java
@@ -3,16 +3,39 @@ package Habitation;
 public class DemoHabitation {
 	public static void main(String[] args) {
         
-		HabitationIndividuelle maison1 = new HabitationIndividuelle("Adrien Sauvage", "123 Rue de la Maison", 150.0, 4, true);
-        HabitationIndividuelle maison2 = new HabitationIndividuelle("Agustina Solimando", "123 Avenue Maison", 120.0, 3, false);
+		Habitation habitation1 = new Habitation("Adrien Sauvage", "142 rue du Routier", 90.3);
+        Habitation habitation2 = new Habitation("Agustina Solimando", "456 Avenue Principale", 75.2);
+		
+        System.out.println("Habitation 1");
+        habitation1.Affiche();
+        System.out.println("Montant de l'impôt : " + habitation1.Impot() + " euros");
 
+        System.out.println("\nHabitation 2 :");
+        habitation2.Affiche();
+        System.out.println("Montant de l'impôt : " + habitation2.Impot() + " euros");
         
-        System.out.println("Maison 1 :");
+        
+		HabitationIndividuelle maison1 = new HabitationIndividuelle("Shakira", "123 Rue de la Maison", 420, 4, true);
+        HabitationIndividuelle maison2 = new HabitationIndividuelle("John Mayer", "456 Avenue Maisonnette", 198.0, 3, false);
+		
+        System.out.println("\nMaison 1 :");
         maison1.Affiche();
         System.out.println("Montant de l'impôt : " + maison1.Impot() + " euros");
 
         System.out.println("\nMaison 2 :");
         maison2.Affiche();
         System.out.println("Montant de l'impôt : " + maison2.Impot() + " euros");
+        
+        HabitationProfessionnelle entreprise1 = new HabitationProfessionnelle("Apple .co", "789 Boulevard Entreprise", 500.0, 15);
+        HabitationProfessionnelle entreprise2 = new HabitationProfessionnelle("Google .org", "101 Rue Technologie", 750.0, 8);
+
+        
+        System.out.println("\nEntreprise 1 :");
+        entreprise1.Affiche();
+        System.out.println("Montant de l'impôt : " + entreprise1.Impot() + " euros");
+
+        System.out.println("\nEntreprise 2 :");
+        entreprise2.Affiche();
+        System.out.println("Montant de l'impôt : " + entreprise2.Impot() + " euros");
     }
 }
--- src/Habitation/HabitationProfessionnelle.java
+++ src/Habitation/HabitationProfessionnelle.java
@@ -2,4 +2,27 @@ package Habitation;
 
 public class HabitationProfessionnelle extends Habitation {
 	private int NbEmployes;
+	
+	public HabitationProfessionnelle(String P, String A, double S, int NbEmployes) {
+        super(P, A, S);
+        this.NbEmployes = NbEmployes;
+    }
+	
+	@Override
+    public double Impot() {
+        double impotBase = 0;
+        if (NbEmployes > 0) {
+            impotBase = (NbEmployes / 10) * 1000;
+        }
+        return impotBase + super.Impot();
+    }
+	
+	@Override
+	public void Affiche() {
+        super.Affiche();
+        System.out.println("Nombre d'employés : " + NbEmployes);
+    }
+	
+	
+	
 }
